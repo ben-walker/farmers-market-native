e9b994da0763cd1d2be3a497ca195084bcd32ed3
Ben Walker <bwalke05@gmail.com>
2021-08-14 12:56:28 -0400

    Fix lint errors

--- App.tsx
+++ App.tsx
@@ -2,18 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const App = () => (
+const App = (): JSX.Element => (
   <View style={styles.container}>
     <Text>Open up App.tsx to start working on your app!</Text>
     <StatusBar style="auto" />
   </View>
 );
 
+const white = "#fff"; // TODO: Global theming
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    backgroundColor: white,
+    flex: 1,
     justifyContent: "center",
   },
 });
