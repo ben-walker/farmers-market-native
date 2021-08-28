c34b7be0b20fe2842abb67cadd1a51686b383ec7
Ben Walker <bwalke05@gmail.com>
2021-08-28 19:27:18 -0400

    Define tab bar icons

--- src/features/navigation/BottomTabs.tsx
+++ src/features/navigation/BottomTabs.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 
@@ -12,7 +13,26 @@ export type BottomTabsParamList = {
 const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export const BottomTabs = (): JSX.Element => (
-  <Tab.Navigator initialRouteName="Browse">
+  <Tab.Navigator
+    initialRouteName="Browse"
+    screenOptions={({ route }) => ({
+      tabBarIcon: function icon({ color, size }) {
+        let iconName: keyof typeof Ionicons.glyphMap;
+
+        switch (route.name) {
+          case "Browse":
+            iconName = "albums";
+            break;
+
+          case "Profile":
+            iconName = "person-circle";
+            break;
+        }
+
+        return <Ionicons name={iconName} color={color} size={size} />;
+      },
+    })}
+  >
     <Tab.Screen name="Browse" component={Browse} />
     <Tab.Screen name="Profile" component={Profile} />
   </Tab.Navigator>
