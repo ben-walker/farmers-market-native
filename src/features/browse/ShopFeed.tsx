e3f3fb53d8c15cbe0436c94a80da0f607ed67d1e
Ben Walker <bwalke05@gmail.com>
2021-08-29 13:56:55 -0400

    Scaffold shops list

--- src/features/browse/ShopFeed.tsx
+++ src/features/browse/ShopFeed.tsx
@@ -1,3 +1,20 @@
+import { useShopsQuery } from "@generated/graphql-codegen";
 import React from "react";
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const ShopFeed = (): JSX.Element => <></>;
+export const ShopFeed = (): JSX.Element => {
+  const shopsResult = useShopsQuery();
+
+  const renderItem = () => <></>;
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={shopsResult.data?.shops}
+        renderItem={renderItem}
+        keyExtractor={(shop) => shop.id}
+      />
+    </SafeAreaView>
+  );
+};
