553773311fe89c1caffee8827a9364a88df51aad
Ben Walker <bwalke05@gmail.com>
2021-08-31 21:06:09 -0400

    Add RouteProp type

--- src/features/bottom-tabs/types.ts
+++ src/features/bottom-tabs/types.ts
@@ -1,9 +1,15 @@
 import { Ionicons } from "@expo/vector-icons";
+import { RouteProp } from "@react-navigation/native";
 
 export type BottomTabsParamList = {
   Browse: undefined;
   Profile: undefined;
 };
+
+export type BottomTabsRouteProp = RouteProp<
+  BottomTabsParamList,
+  keyof BottomTabsParamList
+>;
 
 export type TabBarIconMap = {
   [key in keyof BottomTabsParamList]: keyof typeof Ionicons.glyphMap;
