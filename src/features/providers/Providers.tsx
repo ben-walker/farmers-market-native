d3f6e1c7ba2c92cc00b9b337bd0dc2c8a4aca796
Ben Walker <bwalke05@gmail.com>
2021-09-03 13:04:05 -0400

    Use OS theme

--- src/features/providers/Providers.tsx
+++ src/features/providers/Providers.tsx
@@ -1,6 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
+import React, { useMemo } from "react";
+import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { apolloClient } from "./apollo";
@@ -9,10 +14,19 @@ type ProvidersProps = {
   children: React.ReactNode;
 };
 
-export const Providers: React.FC<ProvidersProps> = ({ children }) => (
-  <ApolloProvider client={apolloClient}>
-    <SafeAreaProvider>
-      <NavigationContainer>{children}</NavigationContainer>
-    </SafeAreaProvider>
-  </ApolloProvider>
-);
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  const colorScheme = useColorScheme();
+
+  const theme = useMemo(
+    () => (colorScheme === "dark" ? DarkTheme : DefaultTheme),
+    [colorScheme]
+  );
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <SafeAreaProvider>
+        <NavigationContainer theme={theme}>{children}</NavigationContainer>
+      </SafeAreaProvider>
+    </ApolloProvider>
+  );
+};
