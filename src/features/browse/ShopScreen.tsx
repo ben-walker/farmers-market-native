21ddbc8a2302a1e183f4743da00b04c6d0717294
Ben Walker <bwalke05@gmail.com>
2021-09-06 11:44:14 -0400

    Query for shop by id

--- src/features/browse/ShopScreen.tsx
+++ src/features/browse/ShopScreen.tsx
@@ -1,11 +1,18 @@
 import { useRoute } from "@react-navigation/native";
 import React from "react";
 
+import { useGetShopQuery } from "../../graphql-codegen";
 import { ScreenContainer } from "../components";
 import { BrowseStackRouteProp } from "./types";
 
 export const ShopScreen: React.FC = () => {
-  const route = useRoute<BrowseStackRouteProp<"Shop">>();
+  const {
+    params: { id },
+  } = useRoute<BrowseStackRouteProp<"Shop">>();
+
+  const shopResult = useGetShopQuery({
+    variables: { shopWhere: { id } },
+  });
 
   return <ScreenContainer></ScreenContainer>;
 };
